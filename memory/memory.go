f731697c50902d65f051bf39f3fb5929185ecb80
Paul Sonkoly <sonkoly.pal@gmail.com>
2024-04-11 10:41:08 +0100

    impl

--- memory/memory.go
+++ memory/memory.go
@@ -57,16 +57,35 @@ func New() *Type {
 // Clone does a memory copy for context switching.
 //
 // The clone would point to the same global, same closure, and the last frame
-// of the stack will be deep copied.
-func (m *Type) Clone() *Type {
+// of the stack will be deep copied. reuse can be nil, when it's not it's
+// resources are re-used to create a new memory.
+func (m *Type) Clone(reuse *Type) *Type {
+	var newStackSize int
+	var newStack []value.Type
+
 	if len(m.fp) < 2 {
-		return &Type{sp: m.sp, fp: []int{}, global: m.global, closure: m.closure, stack: []value.Type{}}
+		newStackSize = minStackSize
+	} else {
+		newStackSize = max(m.sp-m.fp[len(m.fp)+localFP], minStackSize)
 	}
+
+	if reuse != nil {
+		if len(reuse.stack) < newStackSize {
+			reuse.growStack(newStackSize - len(reuse.stack))
+		}
+		newStack = reuse.stack
+	} else {
+		newStack = make([]value.Type, newStackSize)
+	}
+
+	if len(m.fp) < 2 {
+		return &Type{sp: 0, fp: []int{}, global: m.global, closure: m.closure, stack: newStack}
+	}
+
 	fp := m.fp[len(m.fp)+localFP]
 	le := m.fp[len(m.fp)+localFE]
-	newStackSize := max(m.sp-fp, minStackSize)
-	newStack := make([]value.Type, newStackSize)
 	copy(newStack, m.stack[fp:m.sp])
+
 	return &Type{sp: m.sp - fp, fp: []int{0, le - fp}, global: m.global, closure: m.closure, stack: newStack}
 }
 
--- vm/vm.go
+++ vm/vm.go
@@ -3,6 +3,7 @@ package vm
 
 import (
 	"bufio"
+	"container/list"
 	"errors"
 	"fmt"
 	"log"
@@ -29,13 +30,14 @@ type context struct {
 }
 
 type Type struct {
-	ctx *context        // ctx is the context tree
-	CR  compresult.Type // cr is the compilation result
+	freeList *list.List      // freed memory objects
+	ctx      *context        // ctx is the context tree
+	CR       compresult.Type // cr is the compilation result
 }
 
 // New creates a new virtual machine using memory from m and code and data from cr.
 func New(m *memory.Type, cr compresult.Type) *Type {
-	return &Type{ctx: &context{m: m, children: []*context{}}, CR: cr}
+	return &Type{freeList: list.New(), ctx: &context{m: m, children: []*context{}}, CR: cr}
 }
 
 // Run executes the run loop.
@@ -405,17 +407,27 @@ func (vm *Type) Run(retResult bool) (value.Type, error) {
 			jmp := instr.Src0Addr()
 			ctxp.ip = ip + jmp - 1
 
-			m = m.Clone()
+			var free *memory.Type
+			front := vm.freeList.Front()
+			if front != nil {
+				vm.freeList.Remove(front)
+				free = front.Value.(*context).m
+			}
+			m = m.Clone(free)
 			childCtx := context{m: m, parent: ctxp, children: make([]*context, 0)}
 			ctxp.children = append(ctxp.children, &childCtx)
 			ctxp = &childCtx
 
 		case bytecode.RCONT:
+			last := ctxp.children[len(ctxp.children)-1]
+			vm.freeList.PushFront(last)
 			ctxp.children = ctxp.children[:len(ctxp.children)-1]
 
 		case bytecode.DCONT:
 			ctxp = ctxp.parent
 			if len(ctxp.children) > 0 {
+				last := ctxp.children[len(ctxp.children)-1]
+				vm.freeList.PushFront(last)
 				ctxp.children = ctxp.children[:len(ctxp.children)-1]
 			}
 			m = ctxp.m
