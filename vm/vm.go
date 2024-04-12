paulsonkoly/calc

commit 64478918aaf2574fd5c3a31a543f87e3ca4fea39

Pfor (#25)
* paralell for attempt.

Due to for loop embedding uses child contexts, it's difficult to work
out what context to switch to when a single for loop can create multiple
contexts.

* getting it working

* sigh

* sigh2

* some fixes

* fix workflows

* gofmt

* sigh again

* glimmer of hope

* sigh

* is this finally it??

* some readme updates

* trying italic for output

* trying red language as syntax highlight

* scheme

* trying quotes instead of italics

* more styling

* further styling

* more styling

* more styling

* some coderabbit suggestions

* Fix paralell iterations when they don't finish together. Added some
tests

vm/vm.go
@@ -15,6 +15,8 @@ import (
 	"github.com/paulsonkoly/calc/types/bytecode"
 	"github.com/paulsonkoly/calc/types/compresult"
 	"github.com/paulsonkoly/calc/types/value"
+
+	"github.com/kamstrup/intmap"
 )
 
 var (
@@ -22,32 +24,42 @@ var (
 	ErrArity      = errors.New("arity mismatch")
 )
 
+const (
+	minAllocContexts = 16
+)
+
 type context struct {
-	ip       int          // instruction pointer
-	m        *memory.Type // variables
-	parent   *context     // parent context
-	children []*context   // child contexts
+	ip       int                           // instruction pointer
+	m        *memory.Type                  // variables
+	parent   *context                      // parent context
+	children *intmap.Map[uint64, *context] // child contexts
 }
 
 type Type struct {
-	ctx *context        // ctx is the context tree
-	CR  compresult.Type // cr is the compilation result
+	main *context        // main context
+	CR   compresult.Type // cr is the compilation result
 }
 
 // New creates a new virtual machine using memory from m and code and data from cr.
 func New(m *memory.Type, cr compresult.Type) *Type {
-	return &Type{ctx: &context{m: m, children: []*context{}}, CR: cr}
+	contexts := intmap.New[uint64, *context](minAllocContexts)
+	main := context{m: m, children: contexts}
+	return &Type{main: &main, CR: cr}
 }
 
 // Run executes the run loop.
 // nolint:maintidx // the only thing we care about here is making it faster
 func (vm *Type) Run(retResult bool) (value.Type, error) {
-	ctxp := vm.ctx
+	ctxp := vm.main
+
 	m := ctxp.m
 	ip := ctxp.ip
 	ds := vm.CR.DS
 	cs := vm.CR.CS
+
+	// temp/accumulator "register"
 	tmp := value.Nil
+
 	var err error
 
 	freeList := list.New()
@@ -56,7 +68,7 @@ func (vm *Type) Run(retResult bool) (value.Type, error) {
 		instr := (*cs)[ip]
 
 		// TODO allow tracing flag
-		// fmt.Printf("%8d | %8p | %v\n", ip, m, instr)
+		// fmt.Printf("%8d | %8p | %v\n", ip, ctxp, instr)
 
 		opCode := instr.OpCode()
 
@@ -406,38 +418,60 @@ func (vm *Type) Run(retResult bool) (value.Type, error) {
 
 		case bytecode.CCONT:
 			jmp := instr.Src0Addr()
+			ctxID := instr.Src1Addr()
+			ctxHash := hashContext(m, ctxID)
 			ctxp.ip = ip + jmp - 1
 
 			var free *memory.Type
 			front := freeList.Front()
 			if front != nil {
 				freeList.Remove(front)
-				free = front.Value.(*context).m
+				free = front.Value.(*memory.Type)
 			}
 			m = m.Clone(free)
-			childCtx := context{m: m, parent: ctxp, children: make([]*context, 0)}
-			ctxp.children = append(ctxp.children, &childCtx)
-			ctxp = &childCtx
+
+			childCtx := &context{m: m, parent: ctxp, children: intmap.New[uint64, *context](minAllocContexts)}
+
+			ctxp.children.Put(ctxHash, childCtx)
+			ctxp = childCtx
 
 		case bytecode.RCONT:
-			last := ctxp.children[len(ctxp.children)-1]
-			freeList.PushFront(last)
-			ctxp.children = ctxp.children[:len(ctxp.children)-1]
+			lo := instr.Src0Addr()
+			hi := instr.Src1Addr()
+			for i := lo; i <= hi; i++ {
+				hsh := hashContext(m, i)
+				if child, ok := ctxp.children.Get(hsh); ok {
+					deleteContext(child, freeList)
+					ctxp.children.Del(hsh)
+				}
+			}
 
 		case bytecode.DCONT:
-			ctxp = ctxp.parent
-			if len(ctxp.children) > 0 {
-				last := ctxp.children[len(ctxp.children)-1]
-				freeList.PushFront(last)
-				ctxp.children = ctxp.children[:len(ctxp.children)-1]
+			if ctxp.parent != nil {
+				ctxp = ctxp.parent
+				m = ctxp.m
 			}
-			m = ctxp.m
+
+			lo := instr.Src0Addr()
+			hi := instr.Src1Addr()
+			for i := lo; i <= hi; i++ {
+				hsh := hashContext(m, i)
+				if child, ok := ctxp.children.Get(hsh); ok {
+					deleteContext(child, freeList)
+					ctxp.children.Del(hsh)
+				}
+			}
 
 		case bytecode.SCONT:
+			ctxID := instr.Src0Addr()
 			ctxp.m = m
 			ctxp.ip = ip
+			var ok bool
 
-			ctxp = ctxp.children[len(ctxp.children)-1]
+			ctxp, ok = ctxp.children.Get(hashContext(m, ctxID))
+			if !ok {
+				log.Panicf("context not found %016x\n", hashContext(m, ctxID))
+			}
 
 			m = ctxp.m
 			ip = ctxp.ip
@@ -570,12 +604,22 @@ func (vm *Type) dumpStack(ctx *context, ip int, err error, values ...value.Type)
 		fmt.Printf("memory context %08p\n", ctx)
 		m := ctx.m
 		m.DumpStack(vm.CR.Dbg)
-		// reset state for the next run
-		m.Reset()
-		ctx.children = []*context{}
-		ctx.ip = len(*vm.CR.CS)
-		vm.ctx = ctx
 	}
+	// reset state for the next run
+	vm.main.children.Clear()
 
 	return value.Nil, err
+}
+
+func hashContext(m *memory.Type, id int) uint64 {
+	return (uint64(m.CallDepth()) << 15) ^ uint64(id)
+}
+
+func deleteContext(ctxp *context, freeList *list.List) {
+	ctxp.children.ForEach(func(_ uint64, child *context) bool {
+		deleteContext(child, freeList)
+		return true
+	})
+
+	freeList.PushFront(ctxp.m)
 }

cmd/calc/calc_test.go
@@ -130,6 +130,29 @@ var testData = [...]TestDatum{
         }
       }
     }`, nil, value.NewInt(23), nil},
+	{"iterator/parallel for",
+		`{
+      c = ""
+      for i, j <- fromto(1,3), elems("ab") {
+        c = c + toa(i) + " " + j + " "
+      }
+    }`, nil, value.NewString("1 a 2 b "), nil},
+	{"iterator/parallel for mismatching finish",
+		`{
+      c = ""
+      for i, j <- fromto(1,5), elems("ab") {
+        c = c + toa(i) + " " + j + " "
+      }
+    }`, nil, value.NewString("1 a 2 b "), nil},
+	{"iterator/parallel for in recursion",
+		`{
+       f = (n) -> {
+         c = 0
+         if n == 0 return 1
+         for i, j <- fromto(1, 3), fromto(10, 13) c = c + i + j + f(n - 1) 
+       }
+       f(2)
+    }`, nil, value.NewInt(76), nil},
 
 	{"function definition", "(n) -> 1", nil, emptyFunction, nil},
 	{"function/no argument", "() -> 1", nil, emptyFunction, nil},

combinator/combinator.go
@@ -86,6 +86,14 @@ func Not(p Parser) Parser {
 	}
 }
 
+// Drop drops the result of p.
+func Drop(p Parser) Parser {
+	return func(input RollbackLexer) ([]Node, *Error) {
+		_, err := p(input)
+		return []Node{}, err
+	}
+}
+
 // Conditional is a pair of parsers. Once Gate succeeds we don't roll back but
 // we are committed to parse with OnSuccess.
 //

combinator/error.go
@@ -7,6 +7,8 @@ type Error struct {
 	message string
 }
 
+func NewError(msg string, from, to int) *Error { return &Error{message: msg, from: from, to: to} }
+
 func (e *Error) Error() string { return e.message }
 
 // From points to the starting position of the token in the buffer.

go.mod
@@ -4,6 +4,7 @@ go 1.22.0
 
 require (
 	github.com/chzyer/readline v1.5.1
+	github.com/kamstrup/intmap v0.4.0
 	github.com/stretchr/testify v1.8.4
 )
 

go.sum
@@ -6,6 +6,8 @@ github.com/chzyer/test v1.0.0 h1:p3BQDXSxOhOG0P9z6/hGnII4LGiEPOYBhs8asl/fC04=
 github.com/chzyer/test v1.0.0/go.mod h1:2JlltgoNkt4TW/z9V/IzDdFaMTM2JPIi26O1pF38GC8=
 github.com/davecgh/go-spew v1.1.1 h1:vj9j/u1bqnvCEfJOwUhtlOARqs3+rkHYY13jYWTU97c=
 github.com/davecgh/go-spew v1.1.1/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
+github.com/kamstrup/intmap v0.4.0 h1:UjRFBZ4O5BoUbUus3C6xZdCJ/9nudHCxweVYC+zUuYE=
+github.com/kamstrup/intmap v0.4.0/go.mod h1:z3uar6/7HP2QxJJoFTWAKsA5k7Uy1UJjAZoT3f62KEE=
 github.com/pmezard/go-difflib v1.0.0 h1:4DBwDE0NGyQoBHbLQYPwSUPoCMWR5BEzIk/f1lZbAQM=
 github.com/pmezard/go-difflib v1.0.0/go.mod h1:iKH77koFhYxTK1pcRnkKkqfTogsbg7gZNVY4sRDYZ/4=
 github.com/stretchr/testify v1.8.4 h1:CcVxjf3Q8PM0mHUKJCdn+eZZtm5yQwehR5yeSVQQcUk=

memory/memory.go
@@ -84,9 +84,15 @@ func (m *Type) Clone(reuse *Type) *Type {
 
 	fp := m.fp[len(m.fp)+localFP]
 	le := m.fp[len(m.fp)+localFE]
+
 	copy(newStack, m.stack[fp:m.sp])
 
 	return &Type{sp: m.sp - fp, fp: []int{0, le - fp}, global: m.global, closure: m.closure, stack: newStack}
+}
+
+// CallDepth is the number of call frames.
+func (m *Type) CallDepth() int {
+	return len(m.fp) / 2
 }
 
 // SetGlobal sets a global variable.

parser/parser.go
@@ -188,7 +188,25 @@ func whileLoop(input c.RollbackLexer) ([]c.Node, *Error) {
 }
 
 func forLoop(input c.RollbackLexer) ([]c.Node, *Error) {
-	return c.Fmap(mkFor, c.Seq(acceptToken("for"), varName, acceptToken("<-"), expression, block))(input)
+	r, err := c.Seq(
+		acceptToken("for"),
+		c.Fmap(mkList, c.And(varName, c.Any(c.Conditional{Gate: c.Drop(acceptToken(",")), OnSuccess: varName}))),
+		acceptToken("<-"),
+		c.Fmap(mkList, c.And(expression, c.Any(c.Conditional{Gate: c.Drop(acceptToken(",")), OnSuccess: expression}))),
+		block)(input)
+	if err != nil {
+		return nil, err
+	}
+	from := input.From()
+	to := input.To()
+
+	varList := r[1].(node.List)
+	expList := r[3].(node.List)
+	if len(varList.Elems) != len(expList.Elems) {
+		return nil, c.NewError("for loop must have the same number of variables and expressions", from, to)
+	}
+
+	return mkFor(r), nil
 }
 
 func returning(input c.RollbackLexer) ([]c.Node, *Error) {

commit ddea680b5fda42a9d7b0efcad770d30ef78faad7

Fix accidental removal of cleanup
Caused interpreter panic.
Regression introed in #25. Repro code:

```scheme
for i <- g() 1
for i <- g() 1
```

vm/vm.go
@@ -606,6 +606,8 @@ func (vm *Type) dumpStack(ctx *context, ip int, err error, values ...value.Type)
 		m.DumpStack(vm.CR.Dbg)
 	}
 	// reset state for the next run
+	vm.main.m.Reset()
+	vm.main.ip = len(*vm.CR.CS)
 	vm.main.children.Clear()
 
 	return value.Nil, err